01f37520f60057faac262cacac65a2445328d693
rhernandezgar <rhernandezgar@centro.edu.mx>
2025-10-13 20:18:54 -0600

    color de background y text cambio

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,7 +1,14 @@
 const toggleThemeBtn = document.getElementById('toggle-theme-btn');
-
+const colorPicker = document.getElementById('color-picker');
+const backgroundPicker = document.getElementById('background-picker');
 toggleThemeBtn.addEventListener('click',function (){
-
 document.body.classList.toggle('dark-mode'); 
 
 })
+colorPicker.addEventListener('change', function() {
+    document.body.style.color = colorPicker.value;
+});
+
+backgroundPicker.addEventListener('change', function() {
+    document.body.style.backgroundColor = backgroundPicker.value;
+});
